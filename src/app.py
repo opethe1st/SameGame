bb978592e8407cbe2a85be7f9e1ce299fae7b4ca
Opemipo Ogunkola <ogunks@live.com>
2018-01-28 16:08:36 +0000

    .

--- src/app.py
+++ src/app.py
@@ -41,11 +41,10 @@ class SameGame:
             self.gui_client.draw_score_board(score=self.board.get_current_score(), highest_score=self.board.get_high_score(), current_move_score=current_move_score)
 
 
-if __name__ == '__main__':
-    num_columns = 16
-    num_rows = 14
+def main(num_columns = 16, num_rows = 14, size=32):
     aScorer = Scorer()  # pylint: disable=invalid-name
     aBoard = Board(num_rows=num_rows, num_columns=num_columns, num_colours=4, scorer=aScorer)  # pylint: disable=invalid-name
-    aGuiClient = PyGameClient(size=32, num_rows=num_rows, num_columns=num_columns, board_position=(10, 10), score_board_height=100, colours=ColourScheme.MONFAVORITE)  # pylint: disable=invalid-name
+    aGuiClient = PyGameClient(size=size, num_rows=num_rows, num_columns=num_columns, board_position=(10, 10), score_board_height=100, colours=ColourScheme.MONFAVORITE)  # pylint: disable=invalid-name
     sameGame = SameGame(board=aBoard, gui_client=aGuiClient)  # pylint: disable=invalid-name
     sameGame.play_game()
+
