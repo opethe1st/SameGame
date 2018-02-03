e132448291513088f7ddab022d002da4a9489c7e
Opemipo <ogunks@live.com>
2018-02-03 20:53:02 +0000

    Add typing hint (#23)
    Add typing hints

--- src/same/model/scorer.py
+++ src/same/model/scorer.py
@@ -1,3 +1,4 @@
+from typing import List
 import shelve
 
 
@@ -5,23 +6,24 @@ class Scorer:
 
     def __init__(self):
         self.current_score = 0
-        with shelve.open('high_score') as db:
-            self.high_score = db.get('high_score', 0)
+        with shelve.open('high_score') as database:
+            self.high_score = database.get('high_score', 0)
 
     def get_current_score(self):
         return self.current_score
 
-    def update_current_score(self, score):
+    def update_current_score(self, score: int):
         self.current_score += score
         return self.current_score
 
-    def get_score(self, balls):  # pylint: disable=unused-argument
-        return len(balls)**2 if len(balls) > 1 else 0
+    def get_score(self, ball_positions: List[tuple]):  # pylint: disable=unused-argument
+        return len(ball_positions)**2 if len(ball_positions) > 1 else 0
 
     def get_high_score(self):
         return self.high_score
 
-    def update_high_score(self, new_high_score):
-        with shelve.open('high_score') as db:
-            db['high_score'] = new_high_score
-
+    def update_high_score(self, new_high_score: int):
+        with shelve.open('high_score') as database:
+            database['high_score'] = new_high_score
+            self.high_score = database['high_score']
+        return self.high_score
